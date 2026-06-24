fd73c9618af302eb276914e08564a099c9afe8bb
hsprajwal <prajwal9066015098@gmail.com>
2026-06-24 12:05:09 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -4,6 +4,7 @@ import logging
 from src.logger import LOG_FILE_PATH
 
 
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
